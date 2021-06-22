0fc160f90677c27e91cb252c9c70e42a94296459
Alejandro Perez <aperez@iramundi.com>
2021-06-22 10:18:46 -0300

    Cuarto Desafio ultima actualizacion

--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -1,27 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export const ItemCount = ({initial, stock, onAdd}) => {
     const [count, setCount] = useState(0);
- 
-    useEffect(() => {
-      // Actualiza el título del documento usando la API del navegador
-      //document.title = `Clickeaste  veces` + { count };
-    });
 
     function CountClick () {
-      setCount(count +1);
       console.log( stock  + " - " + initial + " " + count)
-      if ( stock >= count ){
+      if ( stock > count ){
         alert("Item Agregado")
+        setCount(count +1);
       } else {
         alert("Stock Insuficiente")
-        setCount(count -1);
       }
    }
     return (
     <div>
       <button onClick={CountClick}>+</button>
       <button onClick={CountClick}>-</button>
+      <h1>Cantidad de Articulos {count} </h1>
+      <p></p>
     </div>
     )
 }
--- src/components/itemListContainer/ItemListContainer.js
+++ src/components/itemListContainer/ItemListContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import {useState} from 'react';
 import { ItemCount } from '../../components/itemCount/ItemCount'
 /*import { ItemList } from '../../components/itemList/ItemList'*/
 
 export const ItemListContainer = () => {
-    const stock = 5
-    const initial = 1
+    const [stock, setStock] = useState(5)
+    const [initial, setInitial] = useState(1)
 
     return (
         <>
                 <h1>Tus Productos Pronto Aqui</h1>
             {   /*<ItemList />  */}    
-                <ItemCount initial={initial} stock={stock} />
+                <ItemCount initial={initial} stock={stock}/>
         </>
     )
 }
